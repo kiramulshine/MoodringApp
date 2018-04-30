1c78e768d510bd981492f642c8de27cb6e1e57f1
Kira <kira.mulshine@gmail.com>
2018-04-30 08:39:23 -0400

    Add files via upload

--- www/js/bluetooth.js
+++ www/js/bluetooth.js
@@ -32,78 +32,64 @@ function stringToBytes(string) {
     return array.buffer;
 }
 
-
-
-
-
-// this is Nordic's UART service - need bluefruit's UUID
+// this is Nordic's UART service
 var bluefruit = {
     serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
     txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
     rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e'  // receive is from the phone's perspective
 };
 
-// var deviceList = document.querySelector('#deviceList');
-// var connectButton = document.querySelector('#connectButton');
-// var sendButton = document.querySelector('#sendButton');
-// var mainPage = document.querySelector('#mainPage');
-// var detailPage = document.querySelector('#detailPage');
-// var resultDiv = document.querySelector('#resultDiv');
-// var rgbString = document.querySelector('#rgbString');
-
 var app = {
+
     initialize: function() {
         this.bindEvents();
-        console.log('connecting');
+        detailPage.hidden = false;
     },
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         connectButton.addEventListener('touchstart', this.scanForDevice, false);
-        sendButton.addEventListener('touchstart', this.sendData, false);
+        sendButton.addEventListener('click', this.write, false);
         // disconnectButton.addEventListener('touchstart', this.disconnect, false);
         deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
     },
     onDeviceReady: function() {
         app.scanForDevice();
     },
-    // looking for Bluetooth connection / device --- scanForDevice
-    scanForDevice: function() {
-        deviceList.innerHTML = bluefruit.serviceUUID; // empties the list
+     scanForDevice: function() {
+        deviceList.innerHTML = ''; // empties the list
         ble.scan([bluefruit.serviceUUID], 5, app.onDiscoverDevice, app.onError);
-        // ble.connect([bluefruit.serviceUUID], onConnect, connectFailure);
         // if Android can't find your device try scanning for all devices
         // ble.scan([], 5, app.onDiscoverDevice, app.onError);
     },
-
-    // building a table to show info for scan results
     onDiscoverDevice: function(device) {
         var listItem = document.createElement('li'),
             html = '<b>' + device.name + '</b><br/>' +
                 'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
                 device.id;
 
+        device.id = "0E2EAC28-BD71-C5CE-ECB6-C52F44672D7F";
         listItem.dataset.deviceId = device.id;
         listItem.innerHTML = html;
         deviceList.appendChild(listItem);
-
-        //ble.connect(deviceId, app.onConnect, app.onError);
-
     },
+
     connect: function(e) {
-        var deviceId = e.target.dataset.deviceId,
+        var deviceId = "0E2EAC28-BD71-C5CE-ECB6-C52F44672D7F",
             onConnect = function(peripheral) {
                 app.determineWriteType(peripheral);
 
                 // subscribe for incoming data
                 ble.startNotification(deviceId, bluefruit.serviceUUID, bluefruit.rxCharacteristic, app.onData, app.onError);
                 sendButton.dataset.deviceId = deviceId;
-                disconnectButton.dataset.deviceId = deviceId;
+                // disconnectButton.dataset.deviceId = deviceId;
                 resultDiv.innerHTML = "";
-                app.showDetailPage();
             };
 
         ble.connect(deviceId, onConnect, app.onError);
+
     },
+
+
     determineWriteType: function(peripheral) {
         // Adafruit nRF8001 breakout uses WriteWithoutResponse for the TX characteristic
         // Newer Bluefruit devices use Write Request for the TX characteristic
@@ -121,18 +107,23 @@ var app = {
         }
 
     },
-    onData: function(data) { // data received from Arduino
+
+
+onData: function(data) { // data received from ring
         console.log(data);
         resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + bytesToString(data) + "<br/>";
         resultDiv.scrollTop = resultDiv.scrollHeight;
     },
-    sendData: function(event) { // send data to Arduino
-        
-        var rgbString = document.querySelector('span#rgbString').value;
+
+write: function(event) { // send data to ring
+
+        var enjoyment = window.localStorage.getItem( "enjoyment" );
+        console.log(enjoyment);
 
         var success = function() {
+
             console.log("success");
-            resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + rgbString.value + "<br/>";
+            resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + enjoyment.value + "<br/>";
             resultDiv.scrollTop = resultDiv.scrollHeight;
         };
 
@@ -140,8 +131,8 @@ var app = {
             alert("Failed writing data to the bluefruit le");
         };
 
-        var data = stringToBytes(rgbString.value);
-        var deviceId = event.target.dataset.deviceId;
+        var data = stringToBytes(enjoyment.value);
+        var deviceId = "0E2EAC28-BD71-C5CE-ECB6-C52F44672D7F";
 
         if (app.writeWithoutResponse) {
             ble.writeWithoutResponse(
@@ -158,27 +149,15 @@ var app = {
                 data, success, failure
             );
         }
+    },
 
-    },
-    
-    disconnect: function(event) {
-        var deviceId = event.target.dataset.deviceId;
-        ble.disconnect(deviceId, app.showMainPage, app.onError);
-    },
-    showMainPage: function() {
-        mainPage.hidden = false;
-        detailPage.hidden = false;
-    },
-    showDetailPage: function() {
-        mainPage.hidden = false;
-        detailPage.hidden = false;
-    },
-    onError: function(reason) {
+onError: function(reason) {
         alert("ERROR: " + JSON.stringify(reason)); // real apps should use notification.alert
-    },
-
+    }
 };
 
+// 
+    
 app.initialize();
 
 
